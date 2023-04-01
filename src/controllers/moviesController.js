a9d47143bef2723475795aa3dba0a88ec288012b
OscarChacoma <oschacoma@gmail.com>
2023-03-31 21:01:16 -0300

    Agregamos editar create

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require("express-validator")
 const db = require ("../database/models")
 const Op = db.Sequelize.Op
 
@@ -36,5 +37,45 @@ module.exports = {
         .then(movies=>{
             return res.render("recommendedMovies",{movies})
         })
+    },
+    add: function (req,res) {
+        return res.render ("moviesAdd")
+    },
+    create: function (req,res) {
+        const errors = validationResult(req);
+
+        if(errors.isEmpty()){
+            const { //destructuramos el req.body
+                title, 
+                awards, 
+                release_date, 
+                length, 
+                rating
+            } = req.body;
+
+            db.Movie.create({ //creacion
+                title, 
+                awards, 
+                release_date, 
+                length, 
+                rating
+            })
+            .then((movie)=>{ //devuelve la movie que creo - devuelve el id de la movie que acaba de crear 
+                return res.redirect("/movies")
+            })
+            .catch((arror) => console.log(error)) //muestra un error en caso de tenerlo
+        }else{
+            
+            return res.render ("moviesAdd",{errors: errors.mapped()})
+        }
+    },
+    edit: function(req,res){
+        const MOVIE_ID = req.params.id;
+
+        db.Movie.findByPk(MOVIE_ID) //retorna una promesa
+        .then(Movie=>{  //capturamos la promesa
+            return res.render("moviesEdit",{Movie})
+        })
+        .catch(error=> console.log(error))
     }
 }
---
+++ src/validations/moviesValidator.js
@@ -0,0 +1,11 @@
+const { check } = require ("express-validator");
+
+module.exports=[
+    check("title").notEmpty().withMessage("Title Requerido"),
+    check("rating").notEmpty().withMessage("Rating Requerido"),
+    check("awards").notEmpty().withMessage("Awards Requerido"),
+    check("release_date").notEmpty().withMessage("Release Requerido")
+]
+
+
+//title, rating, awards, realease_date
